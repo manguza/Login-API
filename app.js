b48a635ceeb73b4f3fdb7e6636fb86b7a26b1204
Pruk Homnan <55727979+manguza@users.noreply.github.com>
2023-10-17 19:13:21 +0700

    17/10/2023

--- app.js
+++ app.js
@@ -3,11 +3,15 @@ var cors = require('cors')
 var bodyParser = require('body-parser')
 var app = express()
 var jsonParser = bodyParser.json()
+const bcrypt = require('bcrypt')
+const saltRounds = 10
+var jwt = require('jsonwebtoken');
+const secret = 'FullStack-Login-2023'
 
 app.use(cors())
 
 // get the client
-const mysql = require('mysql2');
+const mysql = require('mysql2')
 
 // create the connection to database
 const connection = mysql.createConnection({
@@ -17,18 +21,56 @@ const connection = mysql.createConnection({
   });
 
 app.post('/register', jsonParser, function (req, res, next) {
+
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+        // Store hash in your password DB.
+        // execute will internally call prepare and query
+        connection.execute(
+            'INSERT INTO users (email, password, fname, lname) VALUES (?, ?, ?, ?)',
+            [req.body.email, hash, req.body.fname, req.body.lname],
+            function(err, results, fields) {
+                if(err) {
+                    res.json({status : 'error', message : err})
+                }
+                res.json({status : 'Ok'})
+            }
+        )
+    })
+    
+})
+
+app.post('/login', jsonParser, function (req, res, next) {
+
     // execute will internally call prepare and query
     connection.execute(
-        'INSERT INTO users (email, password, fname, lname) VALUES (?, ?, ?, ?) ',
-        [req.body.email, req.body.password, req.body.fname, req.body.lname],
-        function(err, results, fields) {
-            if(err) {
-                res.json({status : 'error', message : err})
-            }
-            res.json({status : 'Ok'})
+        'SELECT * FROM users WHERE email = ?',
+        [req.body.email],
+        function(err, users, fields) {
+            if(err) { res.json({status : 'error', message : err}); return}
+            if(users.length == 0) { res.json({status : 'error', message : 'No user found'}); return}
+            bcrypt.compare(req.body.password, users[0].password, function(err, isLogin) {
+                if(isLogin){
+                    var token = jwt.sign({ email : users[0].email }, secret, { expiresIn: '1h' });
+                    res.json({status : 'Ok', message : 'Login Success', token})
+                } else {
+                    res.json({status : 'bad', message : 'Login Fail'})
+                }
+            });
         }
-    );
-  
+    )
+
+})
+
+app.post('/authen', jsonParser, function (req, res, next) {
+
+    try {
+        const token = req.headers.authorization.split(' ')[1]
+        var decoded = jwt.verify(token, secret);
+        res.json({status : 'Ok', decoded})
+    } catch(err) {
+        res.json({status : 'error', message : err.message})
+    }
+    
 })
 
 app.listen(3000, function () {
